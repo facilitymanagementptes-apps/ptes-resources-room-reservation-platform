4d3fabf37bc4fecc8120f5519a0b02fdd211f033
facilitymanagementptes-apps <facilitymanagementptes@gmail.com>
2026-07-27 20:45:53 +0800

    Update resource.py

--- resource.py
+++ resource.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 from st_gsheets_connection import GSheetsConnection
-from st_gsheets_connection import GSheetsConnection
 import pandas as pd
 from datetime import datetime
 import calendar
